227ab57578b053c2a571457efd3054d2888cf6e0
hasegawa-f <hasegawa-f@east-inc.jp>
2019-04-11 15:27:53 +0900

    Add some aliases for fish.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,46 @@
 set -gx fish_user_paths $HOME/.linuxbrew/bin $fish_user_paths
 
-alias vi='/usr/bin/vim'
+alias i='sudo apt install --yes'
+
+# safety
+alias cp='cp -i'
+alias mv='mv -i'
+alias rm='rm -i'
+
+# cd
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+
+# ls
+alias ll='ls -alF'
+alias l='ls -alF'
+
+# tool
+alias b='brew'
+alias c='code'
+alias clip='clip.exe'
+alias d='docker'
 alias open='explorer.exe'
 alias e='explorer.exe .'
+alias l='ls -al'
+alias p='pa aux'
+alias v='vim'
+alias vi='vim'
+
+# ruby
+alias be='bundle exec'
+alias bi='bundle install'
+alias br='bundle exec rspec'
+
+# python
+alias py='python3'
+
+# edit
+alias vimfish='vim ~/.config/fish/config.fish'
+alias vimrc='vim ~/.vimrc'
+alias vimssh='vim ~/.ssh/config'
+alias refish='source ~/.config/fish/config.fish'
 
 function fzf_select_history
     history|fzf|read slct
@@ -16,4 +54,3 @@ end
 function fish_user_key_bindings
   bind \cr 'fzf_select_history (commandline -b)'
 end
-
