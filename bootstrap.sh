b2d2151aff051fb2f16cc9904a5ff6e25b27aa8a
hasefumi23 <hasefumi23@gmail.com>
2023-05-02 13:15:02 +0900

    bootstrap.shを簡素化

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,71 +2,18 @@
 
 # symlinks
 PWD=`pwd`
-DOTFILES=`ls -a`
-IGNOREFILES=( . .. backup bootstrap.sh brew.sh .config README.md .git .gitignore .vim alacritty.yml profile.json settings.json vim_cheat.md vimium-options.json  )
+DOTFILES=$(find . -maxdepth 1 -type f -name ".*")
 
-BACKUPTIME=`date +%F-%H%M%S`
-
-BACKUPDIR="${PWD}/backup/${BACKUPTIME}"
-
-for DOTFILE in ${DOTFILES[@]}
-do
-  for IGNOREFILE in ${IGNOREFILES[@]}
-  do
-    if [ ${DOTFILE} == ${IGNOREFILE} ]
-    then
-      continue 2
-    fi
-  done
-
+for DOTFILE in ${DOTFILES[@]}; do
   SYMLINK="${HOME}/${DOTFILE}"
-  if [ ! -d ${BACKUPDIR} ]
-  then
-    mkdir -p ${BACKUPDIR}
-  fi
-
-  if [ -f ${SYMLINK} ] && [ ! -L ${SYMLINK} ]
-  then
-    cp -pfa ${SYMLINK} ${BACKUPDIR}
-    echo "Move: ${BACKUPDIR}/${SYMLINK}"
-  fi
-
-  if [ ! -L ${SYMLINK} ] || [ ! -e ${SYMLINK} ]
-  then
-    echo "Link: ${PWD}/${DOTFILE} => ${SYMLINK}"
-    rm -Rf ${SYMLINK}
-    ln -fs ${PWD}/${DOTFILE} ${SYMLINK}
-  fi
+  ln -snfv ${PWD}/${DOTFILE} ${SYMLINK}
 done
 
-# for .config/fish directory
-FISHDIR=${HOME}/.config/fish
-mkdir -p ${BACKUPDIR}/.config
-if [ -d ${FISHDIR} ]
-then
-  cp -pfar ${FISHDIR} ${BACKUPDIR}/.config
-  echo "Move: ${BACKUPDIR}${FISHDIR}"
-fi
-
-echo "Link: ${PWD}/.config/fish => ${FISHDIR}"
-rm -Rf ${FISHDIR}
-ln -fs ${PWD}/.config/fish ${HOME}/.config
-
-# neovim config
-ln -fs ${PWD}/.vimrc ~/.config/nvim/init.vim
-
-# starship
-ln -fs ${PWD}/starship.toml ~/.config/starship.toml
-
-# for vim plugin config
-VIMDIR=${HOME}/.vim/rc
-mkdir -p ${BACKUPDIR}/.vim
-if [ -d ${VIMDIR} ]; then
-  cp -pfar ${VIMDIR} ${BACKUPDIR}/.vim
-  echo "Move: ${BACKUPDIR}/.vim/rc"
-fi
-
-mkdir -p ~/.vim
-rm -Rf ${VIMDIR}/rc
-ln -fs ${PWD}/.vim/rc ~/.vim
+# ~/.config
+mkdir -p ~/.config/fish
+ln -snfv ${PWD}/.config/fish/config.fish ~/.config/fish/config.fish
+ln -snfv ${PWD}/.config/fish/fishfile ~/.config/fish/fishfile
+mkdir -p ~/.config/nvim/ && ln -snfv ${PWD}/.vimrc ~/.config/nvim/init.vim
+mkdir -p ~/.vim/rc && ln -snfv ${PWD}/.vim/rc/dein.toml ~/.vim/rc/dein.toml
+ln -snfv ${PWD}/starship.toml ~/.config/starship.toml
 
