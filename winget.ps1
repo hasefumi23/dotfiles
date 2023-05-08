ebd6e1eb457b874998c9d2da61db40ede5c3504a
hasefumi23 <hasefumi23@gmail.com>
2023-05-08 19:17:49 +0900

    add winget.ps1

---
+++ winget.ps1
@@ -0,0 +1,20 @@
+winget install Google.Chrome
+winget install Microsoft.VisualStudioCode
+winget install SlackTechnologies.Slack
+winget install Zoom.Zoom
+winget install Google.JapaneseIME
+winget install DeepL.DeepL
+# ディレクトリを指定する必要があるので、wingetでは入れない
+# winget install AutoHotkey.AutoHotkey
+winget install Cockos.LICEcap
+winget install Wox.Wox
+winget install Microsoft.PowerToys
+winget install Microsoft.WindowsTerminal
+winget install voidtools.Everything
+winget install Obsidian.Obsidian
+winget install Google.Chrome.Canary
+# 見つからない
+# winget install Monosnap
+winget install WinMerge.WinMerge
+winget install 7zip.7zip
+
